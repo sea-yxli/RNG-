8ed5ecd24f8daa6fe3d33a1779ec8e5b58791529
sea-yxli <126269311+sea-yxli@users.noreply.github.com>
2023-03-12 14:02:57 -0400

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,8 @@
+import random
+print("I'm thinking of a number between 1 and 10.")
+guess = input("Guess the number.")
+number = random.randint(1, 10)
+if guess == number:
+  print(f"That's right! My secret number was {number}.")
+else:
+  print(f"Actually, I was thinking of {number}.")
