b3e00c1a673f4c65dfa6d9e8b2f754bedc3b3fde
sea-yxli <126269311+sea-yxli@users.noreply.github.com>
2023-03-12 14:47:35 -0400

    Update randomness.py

--- randomness.py
+++ randomness.py
@@ -1,5 +1,5 @@
 # answers:
-# 1. the new range is from 1 to 5
+# 1. the new range is from 1 to 4
 # the random numbers are now consistent each time you run the code. Therefore, the random.seed() function is used to generate the same random numbers
 # a new sequence of numbers are given, and they also stay consistent each time you run the code with the same integer inside the function random.seed()
 # it is used in Minecraft to ensure that when the player inserts a certain seed, the new world created always looks the same. It allows the player to generate a seemingly random world; while the number of seeds are finite, there are quintillions of possible combinations of integers and alphabetical letters, and thus quintillions of possible seeds. By entering the same seed, a player can generate their world again, before it was modified by them. 
